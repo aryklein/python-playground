13e3d0317f9d5d14f775c2575bd786724229601b
Ary Kleinerman <kleinerman@gmail.com>
2018-01-16 17:50:54 -0300

    Add exercise statement

--- product-of-other-numbers.py
+++ product-of-other-numbers.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
-  
+
+# You have a list of integers, and for each index you want to find the product of every
+# integer except the integer at that index.
+#
+# Write a function get_products_of_all_ints_except_at_index() that takes a list of
+# integers and returns a list of the products.
+#
+# https://www.interviewcake.com/question/python/product-of-other-numbers
+ 
 def get_products_of_all_ints_except_at_index(lst):
     prod = 1
     prod_list = []
